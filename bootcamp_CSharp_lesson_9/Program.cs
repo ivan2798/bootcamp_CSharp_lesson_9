0a3e756e51ec7b8c5233001cda86a7c7bb3f6051
ivan2798 <inge1998@hotmail.com>
2022-10-19 18:13:35 -0500

    Herencia I

---
+++ bootcamp_CSharp_lesson_9/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+
+
+
+// Hello World! program
+namespace bootcamp_CSharp_lesson_9
+{
+    class Program
+    {
+        // intellisense
+
+        static void Main(string[] args)
+        {
+            Caballo Babieca = new Caballo();
+
+            Humano Jorge = new Humano();    
+
+            Gorila Copito = new Gorila();
+
+            Copito.trepar();
+
+        }
+
+    }
+
+    class Mamiferos
+    {
+        public void respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+        public void cuidarCrias()
+        {
+            Console.WriteLine("Crías");
+        }
+    }
+
+    class Caballo : Mamiferos
+    {
+        public void galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar");
+        }
+    }
+
+    class Humano : Mamiferos
+    {
+        public void pensar()
+        {
+            Console.WriteLine("Pensando");
+        }
+    }
+
+    class Gorila: Mamiferos
+    {
+        public void trepar()
+        {
+            Console.WriteLine("Puedo trepar");
+        }
+    }
+
+}
